3d00bbc8e6dfdec7ce014ebf2198d72e8fb05a7d
Caiorossi00 <caio_rossi56@hotmail.com>
2024-06-11 18:11:45 -0300

    Beta 1.0

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 const denominacoes = [
   {
     valor: 100,
-    imagem:
-      "https://www.bcb.gov.br/novasnotas/assets/img/section/100/100_front.jpg",
+    imagem: "./assets/100.jpg",
   },
   {
     valor: 50,
-    imagem:
-      "https://www.bcb.gov.br/novasnotas/assets/img/section/50/50_front.jpg",
+    imagem: "./assets/50.jpg",
   },
   {
     valor: 20,
-    imagem:
-      "https://www.bcb.gov.br/novasnotas/assets/img/section/20/20_front.jpg",
+    imagem: "./assets/20.jpg",
   },
   {
     valor: 10,
-    imagem:
-      "https://www.bcb.gov.br/novasnotas/assets/img/section/10/10_front.jpg",
+    imagem: "./assets/10.jpg",
   },
   {
     valor: 5,
@@ -26,19 +22,28 @@ const denominacoes = [
   },
   {
     valor: 2,
-    imagem:
-      "https://www.bcb.gov.br/novasnotas/assets/img/section/2/2_front.jpg",
+    imagem: "./assets/2.jpg",
   },
   {
     valor: 1,
-    imagem:
-      "https://www.bcb.gov.br/novasnotas/assets/img/section/1/1_front.jpg",
+    imagem: "./assets/1.jpg",
   },
-  { valor: 0.5, imagem: "https://i.imgur.com/t8GX6v3.png" },
-  { valor: 0.25, imagem: "https://i.imgur.com/8zTLI1r.png" },
-  { valor: 0.1, imagem: "https://i.imgur.com/l3t7yJ8.png" },
-  { valor: 0.05, imagem: "https://i.imgur.com/5y99w97.png" },
-  { valor: 0.01, imagem: "https://i.imgur.com/X7VdVJf.png" },
+  {
+    valor: 0.5,
+    imagem: "./assets/0,50.jpg",
+  },
+  {
+    valor: 0.25,
+    imagem: "./assets/0,25.jpg",
+  },
+  {
+    valor: 0.1,
+    imagem: "./assets/0,10.jpg",
+  },
+  {
+    valor: 0.05,
+    imagem: "./assets/0,05.jpg",
+  },
 ];
 
 const quantidades = {};
@@ -47,8 +52,11 @@ let valorTotalEmCaixa = 0;
 document.addEventListener("DOMContentLoaded", criarCamposQuantidades);
 
 function criarCamposQuantidades() {
-  const quantidadesDiv = document.getElementById("quantidades");
-  quantidadesDiv.innerHTML = "";
+  const notasDiv = document.getElementById("quantidades-notas");
+  const moedasDiv = document.getElementById("quantidades-moedas");
+  notasDiv.innerHTML = "";
+  moedasDiv.innerHTML = "";
+
   denominacoes.forEach((denominacao) => {
     const container = document.createElement("div");
     container.style.marginBottom = "10px";
@@ -56,8 +64,6 @@ function criarCamposQuantidades() {
     const img = document.createElement("img");
     img.src = denominacao.imagem;
     img.alt = `${denominacao.valor} Reais`;
-    img.style.width = "200px";
-    img.style.marginRight = "5px";
 
     const input = document.createElement("input");
     input.type = "number";
@@ -67,7 +73,13 @@ function criarCamposQuantidades() {
 
     container.appendChild(img);
     container.appendChild(input);
-    quantidadesDiv.appendChild(container);
+
+    if (denominacao.valor <= 1) {
+      img.classList.add("moeda-menor");
+      moedasDiv.appendChild(container);
+    } else {
+      notasDiv.appendChild(container);
+    }
   });
 }
 
@@ -104,12 +116,14 @@ function calcularTroco() {
   const valorRetirar = valorTotalEmCaixa - valorDesejado;
   const troco = calcularNotasMoedas(valorRetirar);
 
-  if (troco.valorRestante > 0) {
-    alertarTrocoInsuficiente(troco.valorRestante);
-    return;
-  }
+  const valorRetirado = valorRetirar - troco.valorRestante;
 
-  exibirResultados(troco.notasMoedas, valorRetirar, valorDesejado);
+  exibirResultados(
+    troco.notasMoedas,
+    valorRetirado,
+    valorDesejado,
+    troco.valorRestante
+  );
 }
 
 function calcularNotasMoedas(valorRetirar) {
@@ -129,43 +143,7 @@ function calcularNotasMoedas(valorRetirar) {
   return { notasMoedas: troco, valorRestante };
 }
 
-function alertarTrocoInsuficiente(valorRestante) {
-  const motivos = identificarMotivos(valorRestante);
-  const solucoes = sugerirSolucoes(valorRestante);
-
-  let mensagem =
-    "Não é possível retirar o valor desejado com as notas e moedas disponíveis.\n\nMotivos:\n";
-  motivos.forEach((motivo) => (mensagem += `- ${motivo}\n`));
-
-  if (solucoes.length > 0) {
-    mensagem += "\nSoluções alternativas:\n";
-    solucoes.forEach((solucao) => (mensagem += `- ${solucao}\n`));
-  }
-
-  alert(mensagem);
-}
-
-function identificarMotivos(valorRestante) {
-  return denominacoes
-    .filter(
-      (denominacao) =>
-        valorRestante >= denominacao.valor &&
-        quantidades[denominacao.valor] === 0
-    )
-    .map(
-      (denominacao) =>
-        `Faltam notas/moedas de R$ ${denominacao.valor.toFixed(2)}.`
-    );
-}
-
-function sugerirSolucoes(valorRestante) {
-  const solucoes = [];
-  const valorAproximado = Math.floor(valorRestante / 10) * 10;
-  solucoes.push(`Retirar R$ ${valorAproximado.toFixed(2)} (aproximado).`);
-  return solucoes;
-}
-
-function exibirResultados(troco, valorRetirar, valorDesejado) {
+function exibirResultados(troco, valorRetirar, valorDesejado, valorRestante) {
   const elementoExibicao = document.getElementById("exibicao");
   elementoExibicao.innerHTML = "";
 
@@ -178,25 +156,40 @@ function exibirResultados(troco, valorRetirar, valorDesejado) {
 
   const containerImagens = document.createElement("div");
   containerImagens.id = "imagensTroco";
+  containerImagens.style.display = "flex";
+  containerImagens.style.flexWrap = "wrap";
+  containerImagens.style.gap = "10px";
+  containerImagens.style.marginTop = "10px";
 
-  Object.entries(troco).forEach(([valor, quantidade]) => {
+  const sortedDenominacoes = [...denominacoes].sort(
+    (a, b) => b.valor - a.valor
+  );
+
+  sortedDenominacoes.forEach((denominacao) => {
+    const valor = denominacao.valor;
+    const quantidade = troco[valor];
+
     if (quantidade > 0) {
-      const denominacao = denominacoes.find((den) => den.valor == valor);
       const imgContainer = document.createElement("div");
       imgContainer.style.display = "flex";
+      imgContainer.style.flexDirection = "column";
       imgContainer.style.alignItems = "center";
       imgContainer.style.marginBottom = "10px";
 
       const img = document.createElement("img");
       img.src = denominacao.imagem;
       img.alt = `${valor} Reais`;
-      img.style.width = "150px";
-      img.style.marginRight = "10px";
+      if (denominacao.valor <= 1) {
+        img.classList.add("moeda-menor");
+      } else {
+        img.style.width = "150px";
+      }
+      img.style.marginBottom = "5px";
 
       const texto = document.createElement("span");
       texto.textContent = `x${quantidade} = R$ ${(valor * quantidade).toFixed(
         2
-      )}`; // Mostra a quantidade e o valor total da denominação
+      )}`;
 
       imgContainer.appendChild(img);
       imgContainer.appendChild(texto);
@@ -205,4 +198,13 @@ function exibirResultados(troco, valorRetirar, valorDesejado) {
   });
 
   elementoExibicao.appendChild(containerImagens);
+
+  if (valorRestante > 0) {
+    const mensagem = document.createElement("p");
+    mensagem.style.marginTop = "20px";
+    mensagem.textContent = `Não foi possível retirar exatamente o valor desejado. O valor mais próximo possível foi retirado. Valor restante não retirado: R$ ${valorRestante.toFixed(
+      2
+    )}`;
+    elementoExibicao.appendChild(mensagem);
+  }
 }
